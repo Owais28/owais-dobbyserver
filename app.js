c24f0574dbd165e3bd38ec5549dfa1e55a2e1e08
Owais Athar <owaisathar2882@gmail.com>
2022-10-09 12:43:42 +0530

    done

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ var createError = require('http-errors');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config();
-
-// var api = require('./routes/api/main');
-// var app = express();
 var cors = require('cors')
 // var bodyParser = require('body-parser')
 // var multer = require('multer')
@@ -13,123 +10,6 @@ var cors = require('cors')
 // // var Grid = require('gridfs-stream');
 // // var methodOverride = require('method-override');
 var imgModel = require('./models/Image')
-// var fs = require('fs');
-// // var uplaod = require('./middleware/upload')
-// // const GridFSBucket = require("mongodb").GridFSBucket;
-// // const uploadController = require("./controllers/upload");
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, '/temp/images')
-//   },
-//   filename: function (req, file, cb) {
-//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-//     cb(null, file.originalname + '-' + uniqueSuffix)
-//   }
-// })
-
-// var uplaod = multer({ storage : storage })
-// // view engine setup
-// // app.set('views', path.join(__dirname, 'views'));
-// // app.set('view engine', 'ejs');
-
-
-
-
-// //Get the default connection
-// var db = mongoose.connection;
-
-// //Bind connection to error event (to get notification of connection errors)
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-
-// // let gfs;
-// // db.once("open", function () {
-// //   gfs = new mongoose.mongo.GridFSBucket(
-// //     db.db,
-// //     {
-// //       bucketName: 'image'
-// //     }
-// //   )
-// // });
-
-// app.post('/login', uplaod.single('image'), (req, res) => {
-//   console.log(req.body);
-//   const newmage = new imgModel(
-//     {
-//       title: req.body.title,
-//       img: {
-//         data: fs.readFileSync(path.join(__dirname + '/temp/images' + req.file.filename)),
-//         contentType: 'image/png'
-//       },
-//       author: req.body.author
-//     }
-//   )
-//   // newmage.save().then(
-//   //   image => {
-//   //     res.status(200).json(
-//   //       {
-//   //         success: true,
-//   //         image
-//   //       }
-//   //     )
-//   //   }
-//   // ).catch(
-//   //   err =>
-//   //     res.status(500).json(err)
-
-//   // )
-
-//   // console.log(req.body.file)
-//   // res.json(
-//   //   {
-//   //     message : req.body
-//   //   }
-//   //   )
-
-
-//   imgModel.create(obj, (err, item) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     else {
-//       // item.save();
-//       res.redirect('/');
-//     }
-//   });
-//   res.json(
-//     {
-//       message: "Successful!"
-//     }
-//   )
-// })
-
-// // app.post('/login', uploadController.uploadFiles)
-
-// // app.use('/api', api)
-
-// // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.json({
-//     message:
-//       "error"
-//   });
-// });
-
 
 // module.exports = app;
 
